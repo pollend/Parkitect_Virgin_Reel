1d1eea3ea3aa7fa94b5e45fdb10cf98d8d8693ef
Michael Pollind <mpollind@gmail.com>
2016-06-12 12:09:43 -0700

    start

---
+++ AssetBundleManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AssetBundleManager
+{
+	private Main Main {get;set;}
+    public GameObject FrontCarGo;
+    public GameObject BackCarGo;
+
+    public GameObject[] Support_Go;
+    public GameObject[] AngledSupportGo;
+    public GameObject SupportHalf;
+
+
+
+
+	public AssetBundleManager (Main main)
+	{
+		this.Main = main;
+        FrontCarGo = LoadAsset<GameObject> ("Front_Car");
+        BackCarGo = LoadAsset<GameObject> ("Cart");
+
+        Support_Go = new GameObject[] {
+            LoadAsset<GameObject> ("Support_1"),
+            LoadAsset<GameObject> ("Support_2"),
+            LoadAsset<GameObject> ("Support_3 1")
+            };
+
+        AngledSupportGo = new GameObject[] {
+            LoadAsset<GameObject>("Angle_support_1"),
+            LoadAsset<GameObject>("Angle_support_2")
+        };
+
+        SupportHalf = LoadAsset<GameObject> ("Half_Support");
+
+
+
+    }
+
+
+
+	private T  LoadAsset<T>(string prefabName) where T : UnityEngine.Object
+	{
+		try
+		{
+			T asset;
+
+			char dsc = System.IO.Path.DirectorySeparatorChar;
+			using (WWW www = new WWW("file://" + Main.Path + dsc + "assetbundle" + dsc + "MineTrain"))
+			{
+
+				if (www.error != null)
+				{
+					Debug.Log("Loading had an error:" + www.error);
+					throw new Exception("Loading had an error:" + www.error);
+				}
+				if(www.assetBundle == null)
+				{
+					Debug.Log("Loading had an error:" + www.error);
+					throw new Exception("assetBundle is null");
+
+				}
+				AssetBundle bundle = www.assetBundle;
+
+
+				try
+				{
+					asset = bundle.LoadAsset<T>(prefabName);
+					bundle.Unload(false);
+
+					return asset;
+				}
+				catch (Exception e)
+				{
+					UnityEngine.Debug.LogException(e);
+					bundle.Unload(false);
+					return null;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			UnityEngine.Debug.LogException(e);
+			return null;
+		}
+	}
+}
+
+
---
+++ Main.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Main : IMod
+{
+    public string Identifier { get; set; }
+	public static AssetBundleManager AssetBundleManager = null;
+    public static Configuration Configeration = null;
+
+    private List<UnityEngine.Object> registeredObjects = new List<UnityEngine.Object>();
+
+    GameObject hider;
+    public void onEnabled()
+    {
+        return;
+        hider = new GameObject ();
+
+		if (Main.AssetBundleManager == null) {
+
+			AssetBundleManager = new AssetBundleManager (this);
+		}
+
+        TrackedRide selected = null;
+        foreach (Attraction t in AssetManager.Instance.getAttractionObjects ()) {
+            if (t.getUnlocalizedName() == "Ghost Mansion Ride") {
+                selected = (TrackedRide)t;
+                break;
+                    
+            }
+        }
+
+
+
+        TrackedRide trackRider = UnityEngine.Object.Instantiate (selected);
+
+        /*MineTrainSupportInstantiator supportInstaiator = ScriptableObject.CreateInstance<MineTrainSupportInstantiator> ();
+        AssetManager.Instance.registerObject (supportInstaiator);
+        registeredObjects.Add (supportInstaiator);
+        supportInstaiator.baseMaterial = selected.meshGenerator.material;
+*/
+
+        trackRider.canChangeCarRotation = false;
+        trackRider.meshGenerator = selected.meshGenerator;// ScriptableObject.CreateInstance<MinetrainTrackGenerator> ();
+        trackRider.meshGenerator.stationPlatformGO = selected.meshGenerator.stationPlatformGO;
+        trackRider.meshGenerator.material = selected.meshGenerator.material;
+        trackRider.meshGenerator.liftMaterial = selected.meshGenerator.liftMaterial;
+        trackRider.meshGenerator.frictionWheelsGO = selected.meshGenerator.frictionWheelsGO;
+        trackRider.meshGenerator.supportInstantiator =selected.meshGenerator.supportInstantiator;
+        trackRider.meshGenerator.crossBeamGO = selected.meshGenerator.crossBeamGO;
+
+
+        Color[] colors = new Color[] { new Color(63f / 255f, 46f / 255f, 37f / 255f, 1), new Color(43f / 255f, 35f / 255f, 35f / 255f, 1), new Color(90f / 255f, 90f / 255f, 90f / 255f, 1) };
+        trackRider.meshGenerator.customColors = colors;
+        trackRider.meshGenerator.customColors = colors;
+        trackRider.setDisplayName("Car_Train_Ride");
+        trackRider.price = 3600;
+        trackRider.name = "car_ride_GOasdfasdf";
+        AssetManager.Instance.registerObject (trackRider);
+        registeredObjects.Add (trackRider);
+
+        //get car
+        GameObject carGo = UnityEngine.GameObject.Instantiate(Main.AssetBundleManager.BackCarGo);
+        Rigidbody carRigid = carGo.AddComponent<Rigidbody> ();
+        carRigid.isKinematic = true;
+        carGo.AddComponent<BoxCollider> ();
+
+        GameObject frontcarGo = UnityEngine.GameObject.Instantiate(Main.AssetBundleManager.FrontCarGo);
+        Rigidbody frontcarRigid = frontcarGo.AddComponent<Rigidbody> ();
+        frontcarRigid.isKinematic = true;
+        frontcarGo.AddComponent<BoxCollider> ();
+
+        //add Component
+        MineTrainCar frontCar = frontcarGo.AddComponent<MineTrainCar> ();
+        MineTrainCar car = carGo.AddComponent<MineTrainCar> ();
+
+        frontCar.Decorate (true);
+        car.Decorate (false);
+
+        CoasterCarInstantiator coasterCarInstantiator = ScriptableObject.CreateInstance<CoasterCarInstantiator> ();
+        List<CoasterCarInstantiator> trains = new List<CoasterCarInstantiator>();
+
+        coasterCarInstantiator.name = "Car Train@CoasterCarInstantiator";
+        coasterCarInstantiator.defaultTrainLength = 5;
+        coasterCarInstantiator.maxTrainLength = 7;
+        coasterCarInstantiator.minTrainLength = 2;
+        coasterCarInstantiator.carGO = carGo;
+        coasterCarInstantiator.frontCarGO = frontcarGo;
+
+        //register cars
+        AssetManager.Instance.registerObject (frontCar);
+        AssetManager.Instance.registerObject (car);
+        registeredObjects.Add (frontCar);
+        registeredObjects.Add (car);
+        //Offset
+        float CarOffset = .02f;
+        car.offsetBack = CarOffset;
+        frontCar.offsetBack = CarOffset;
+
+        //Restraints
+        RestraintRotationController controller = carGo.AddComponent<RestraintRotationController>();
+        RestraintRotationController controllerFront = frontcarGo.AddComponent<RestraintRotationController>();
+        controller.closedAngles = new Vector3(0,0,120);
+        controllerFront.closedAngles = new Vector3(0, 0, 120);
+
+
+        //Custom Colors
+        Color[] CarColors = new Color[] { new Color(68f / 255, 58f / 255, 50f / 255), new Color(176f / 255, 7f / 255, 7f / 255), new Color(55f / 255, 32f / 255, 12f / 255),new Color(61f / 255, 40f / 255, 19f / 255)};
+
+        MakeRecolorble(frontcarGo, "CustomColorsDiffuse", CarColors);
+        MakeRecolorble(carGo, "CustomColorsDiffuse", CarColors);
+
+        coasterCarInstantiator.displayName = "Car Car";
+        AssetManager.Instance.registerObject (coasterCarInstantiator);
+        registeredObjects.Add (coasterCarInstantiator);
+
+        trains.Add (coasterCarInstantiator);
+
+        trackRider.carTypes = trains.ToArray();
+
+        hider.SetActive (false);
+        carGo.transform.parent = hider.transform;
+        frontcarGo.transform.parent = hider.transform;
+
+
+	}
+
+    private void MakeRecolorble(GameObject GO, string shader, Color[] colors)
+    {
+        CustomColors cc = GO.AddComponent<CustomColors>();
+        cc.setColors(colors);
+
+        foreach (Material material in AssetManager.Instance.objectMaterials)
+        {
+            if (material.name == shader)
+            {
+                SetMaterial(GO, material);
+                break;
+            }
+        }
+
+    }
+
+    private void SetMaterial(GameObject go, Material material)
+    {
+        // Go through all child objects and recolor     
+        Renderer[] renderCollection;
+        renderCollection = go.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer render in renderCollection)
+        {
+            render.sharedMaterial = material;
+        }
+    }
+
+    public void onDisabled()
+    {
+        foreach(UnityEngine.Object o in registeredObjects)
+        {
+            AssetManager.Instance.unregisterObject (o);
+        }
+        UnityEngine.GameObject.DestroyImmediate (hider);
+	}
+
+    public string Name
+    {
+        get { return "Mine Train Coaster"; }
+    }
+
+    public string Description
+    {
+        get { return "Mine Train Coaster"; }
+    }
+
+
+	public string Path { get; set; }
+
+}
+
