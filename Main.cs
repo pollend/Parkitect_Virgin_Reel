99e5037e4724ac371994b3c5b540aff34a62706f
Michael Pollind <mpollind@gmail.com>
2018-03-05 16:50:53 -0800

    tweak track utility

--- Main.cs
+++ Main.cs
@@ -1,18 +1,17 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Linq;
 using TrackedRiderUtility;
+using UnityEngine;
 
 public class Main : IMod
 {
-    public string Identifier { get; set; }
-	public static AssetBundleManager AssetBundleManager = null;
+	public static AssetBundleManager AssetBundleManager;
   
     private TrackRiderBinder binder;
 
     public void onEnabled()
     {
         
-		if (Main.AssetBundleManager == null) {
+		if (AssetBundleManager == null) {
 
 			AssetBundleManager = new AssetBundleManager (this);
 		}
@@ -22,23 +21,23 @@ public class Main : IMod
         VirginiaReelTrackGenerator trackGenerator = binder.RegisterMeshGenerator<VirginiaReelTrackGenerator> (trackedRide);
         TrackRideHelper.PassMeshGeneratorProperties (TrackRideHelper.GetTrackedRide ("Wooden Coaster").meshGenerator,trackedRide.meshGenerator);
 
-        trackGenerator.crossBeamGO = GameObjectHelper.SetUV (Main.AssetBundleManager.SideCrossBeamGo, 15, 14);
+        trackGenerator.crossBeamGO = GameObjectHelper.SetUV (AssetBundleManager.SideCrossBeamGo, 15, 14);
 
         trackedRide.price = 1200;
         trackedRide.maxBankingAngle = 0;
         trackedRide.min90CurveSize = 1;
         trackedRide.carTypes = new CoasterCarInstantiator[]{ };
-        trackedRide.meshGenerator.customColors = new Color[] { new Color(63f / 255f, 46f / 255f, 37f / 255f, 1), new Color(43f / 255f, 35f / 255f, 35f / 255f, 1), new Color(90f / 255f, 90f / 255f, 90f / 255f, 1) };
+        trackedRide.meshGenerator.customColors = new[] { new Color(63f / 255f, 46f / 255f, 37f / 255f, 1), new Color(43f / 255f, 35f / 255f, 35f / 255f, 1), new Color(90f / 255f, 90f / 255f, 90f / 255f, 1) };
         trackedRide.canChangeCarRotation = true;
 
         CoasterCarInstantiator coasterCarInstantiator = binder.RegisterCoasterCarInstaniator<CoasterCarInstantiator> (trackedRide, "VirginiaReelInstantiator", "Virginia Reel Car", 1, 1, 1);
-        VirginiaReelCar virginiaReelCar =  binder.RegisterCar<VirginiaReelCar> ( Main.AssetBundleManager.CartGo,"VirginiaReelCar", .3f,.1f,true, new Color[] { 
+        VirginiaReelCar virginiaReelCar =  binder.RegisterCar<VirginiaReelCar> ( AssetBundleManager.CartGo,"VirginiaReelCar", .3f,.1f,true, new[] { 
             new Color(71f / 255, 71f / 255, 71f / 255), 
             new Color(176f / 255, 7f / 255, 7f / 255), 
             new Color(26f / 255, 26f / 255, 26f / 255)}
         );
-        coasterCarInstantiator.carGO = virginiaReelCar.gameObject;
-        coasterCarInstantiator.carGO.AddComponent<RestraintRotationController>().closedAngles = new Vector3(0,0,120);
+        coasterCarInstantiator.vehicleGO = virginiaReelCar;
+        coasterCarInstantiator.vehicleGO.gameObject.AddComponent<RestraintRotationController>().closedAngles = new Vector3(0,0,120);
 
         binder.Apply ();
 
@@ -60,18 +59,19 @@ public class Main : IMod
         binder.Unload ();
 	}
 
-    public string Name
-    {
-        get { return "Virginia Reel"; }
-    }
+    public string Name => "Virginia Reel";
 
-    public string Description
-    {
-        get { return "The Virginia reel  uses side friction like tracks. The tubs, have inward facing seating which spin freely on a chassis. The tubs are spun as they contacted the edges of the trough."; }
-    }
+    public string Description => "The Virginia reel  uses side friction like tracks. The tubs, have inward facing seating which spin freely on a chassis. The tubs are spun as they contacted the edges of the trough.";
 
-
-	public string Path { get; set; }
-
+	string IMod.Identifier => "VirginiaReel";
+	
+	
+	public string Path
+	{
+		get
+		{
+			return ModManager.Instance.getModEntries().First(x => x.mod == this).path;
+		}
+	}
 }
 
--- VirginiaReelCar.cs
+++ VirginiaReelCar.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using TrackedRiderUtility;
 using UnityEngine;
-using TrackedRiderUtility;
-
 
 public class VirginiaReelCar : BaseCar
 {
@@ -9,63 +7,70 @@ public class VirginiaReelCar : BaseCar
 	private const float radius = .4f;
 	private const float timeSpentRotating = 3f;
 
-	private float rotational_speed = 0f;
+	private float rotational_speed;
 	private float maxRotation = 70f;
 
 
-	protected override void Awake ()
+	protected override void Awake()
 	{
-        this.carRotationAxis = transform.Find ("rotator");
-		frontAxis = transform.Find ("frontAxis");
-		backAxis = transform.Find ("backAxis");
-		base.Awake ();
+		carRotationAxis = transform.Find("rotator");
+		frontAxis = transform.Find("frontAxis");
+		backAxis = transform.Find("backAxis");
+		base.Awake();
 	}
 
-	protected override void onRepositionAxis (float frontAxisPosition, float backAxisPosition)
+
+	public override void reposition(float deltaTime, float position, int lane, Car previousCar)
 	{
-		if (this.train != null) {
-			Vector3 tangent_axis = this.track.getTangentPoint (backAxisPosition);
-			Vector3 next_tangent_axis = this.track.getTangentPoint (frontAxisPosition);
+		if (train != null)
+		{
+			Vector3 tangentAxis = this.track.getTangentPoint(currentTrackPosition);
+			Vector3 nextTangentAxis = this.track.getTangentPoint(currentBackTrackPosition);
 
-			Vector3 normal_axis = this.track.getNormalPoint (backAxisPosition);
-			Vector3 binormal = Vector3.Cross (normal_axis, tangent_axis).normalized;
+			Vector3 normalAxis = this.track.getNormalPoint(currentTrackPosition);
+			Vector3 binormal = Vector3.Cross(normalAxis, tangentAxis).normalized;
 
 
-			TrackSegment4 track = this.track.trackSegments [(int)backAxisPosition];
-			if (!(track is Station)) {
-				float angle = MathHelper.AngleSigned (tangent_axis, next_tangent_axis, normal_axis);
-				rotational_speed += ((Mathf.Sign (angle) * Mathf.Sin (Mathf.Abs (angle)) * this.train.velocity * Time.deltaTime * 2f) / (.2f * Mathf.PI)) * Mathf.Rad2Deg * Time.deltaTime;
-                if(Mathf.Abs (angle) < .2f)
-                rotational_speed -= this.rotational_speed * .6f * Time.deltaTime;
+			TrackSegment4 track = this.track.trackSegments[(int) currentTrackPosition];
+			if (!(track is Station))
+			{
+				float angle = MathHelper.AngleSigned(tangentAxis, nextTangentAxis, normalAxis);
+				rotational_speed +=
+				((Mathf.Sign(angle) * Mathf.Sin(Mathf.Abs(angle)) * train.velocity * Time.deltaTime * 2f) /
+				 (.2f * Mathf.PI)) * Mathf.Rad2Deg * Time.deltaTime;
+				if (Mathf.Abs(angle) < .2f)
+					rotational_speed -= rotational_speed * .6f * Time.deltaTime;
 
-                if (Mathf.Abs(this.rotational_speed) > maxRotation )
-                    this.rotational_speed = maxRotation * Mathf.Sign(this.rotational_speed);
+				if (Mathf.Abs(rotational_speed) > maxRotation)
+					rotational_speed = maxRotation * Mathf.Sign(rotational_speed);
 
-				float additional_rotation = ((Mathf.Sign (angle) * Mathf.Sin (Mathf.Abs (angle)) * this.train.velocity) / (.2f * Mathf.PI)) * Mathf.Rad2Deg * Time.deltaTime;
+				float additionalRotation =
+					((Mathf.Sign(angle) * Mathf.Sin(Mathf.Abs(angle)) * train.velocity) / (.2f * Mathf.PI)) * Mathf.Rad2Deg *
+					Time.deltaTime;
 
-                this.carRotationAxis.localRotation *= Quaternion.AngleAxis (additional_rotation+rotational_speed, Vector3.up);
-			} else {
-				rotational_speed -= this.rotational_speed * .6f * Time.deltaTime;
+				carRotationAxis.localRotation *= Quaternion.AngleAxis(additionalRotation + rotational_speed, Vector3.up);
+			}
+			else
+			{
+				rotational_speed -= rotational_speed * .6f * Time.deltaTime;
 
-                if (Quaternion.Angle (Quaternion.identity, this.carRotationAxis.localRotation) > 5f) {
-                    this.carRotationAxis.localRotation *= Quaternion.AngleAxis (rotational_speed + Time.deltaTime * 40f, Vector3.up);
+				if (Quaternion.Angle(Quaternion.identity, carRotationAxis.localRotation) > 5f)
+				{
+					carRotationAxis.localRotation *= Quaternion.AngleAxis(rotational_speed + Time.deltaTime * 40f, Vector3.up);
 				}
 			}
 		}
-
-
-		base.onRepositionAxis (frontAxisPosition, backAxisPosition);
-
-
 	}
 
-	public override bool isReadyForLettingGuestsInAndOut ()
+
+
+	public override bool isReadyForLettingGuestsInAndOut()
 	{
-        if (Quaternion.Angle (Quaternion.identity, this.carRotationAxis.localRotation) > 5f) {
+		if (Quaternion.Angle(Quaternion.identity, carRotationAxis.localRotation) > 5f)
+		{
 			return false;
 		}
-		return base.isReadyForLettingGuestsInAndOut ();
+		return base.isReadyForLettingGuestsInAndOut();
 	}
-
 
 }
